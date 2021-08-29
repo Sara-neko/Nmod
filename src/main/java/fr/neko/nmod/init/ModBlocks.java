e47aadd7c1b9937aba1b0aa2cf23698b28839ea9
GarDLaw <trytrybg@gmail.com>
2021-08-29 16:35:34 +0200

    Law: creation de le creative tab

--- src/main/java/fr/neko/nmod/init/ModBlocks.java
+++ src/main/java/fr/neko/nmod/init/ModBlocks.java
@@ -1,6 +1,7 @@
 package fr.neko.nmod.init;
 
 import fr.neko.nmod.Nmod;
+import fr.neko.nmod.utils.ModItemGroups;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -21,12 +22,13 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> CUIVRE = createBlock("cuivre", () -> new Block(AbstractBlock.Properties.of(Material.METAL).strength(3f,15f).harvestTool(ToolType.PICKAXE).harvestLevel(3)));
     public static final RegistryObject<Block> EDARITE_ORE = createBlock("edarite_ore",() -> new Block(AbstractBlock.Properties.of(Material.METAL).strength(3f,15f).harvestTool(ToolType.PICKAXE).harvestLevel(3)));
+    public static final RegistryObject<Block> SAKURA_LOG = createBlock("sakura_log",()  -> new Block(AbstractBlock.Properties.of(Material.WOOD).strength(3f, 15f).harvestTool(ToolType.AXE).harvestLevel(0)));
 
 
     public static RegistryObject<Block> createBlock(String name, Supplier<? extends Block> supplier)
     {
         RegistryObject<Block> block = BLOCKS.register(name, supplier);
-        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(ModItemGroups.Nmod_TAB)));
         return block;
     }
 }
